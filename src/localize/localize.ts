8fb9d9984f6073e46b92fac1951624ee5773eec0
marksie1988 <marksie1988@users.noreply.github.com>
2023-08-14 16:09:06 +0100

    feat: start localize & change text numbers to numbers

---
+++ src/localize/localize.ts
@@ -0,0 +1,26 @@
+import * as en from './languages/en.json';
+
+const languages: any = {
+	en: en,
+};
+
+export function localize(string: string, search = '', replace = '') {
+	const lang = (localStorage.getItem('selectedLanguage') || 'en').replace(/['"]+/g, '').replace('-', '_');
+
+	let translated: string;
+
+	try {
+		translated = string.split('.').reduce((o, i) => o[i], languages[lang]);
+	} catch (e) {
+		translated = string.split('.').reduce((o, i) => o[i], languages['en']);
+	}
+
+	if (translated === undefined) {
+		translated = string.split('.').reduce((o, i) => o[i], languages['en']);
+	}
+
+	if (search !== '' && replace !== '') {
+		translated = translated.replace(search, replace);
+	}
+	return translated;
+}
