3eedda101217313de094d94d3705b3004af373bd
marksie1988 <marksie1988@users.noreply.github.com>
2023-08-17 19:25:54 +0000

    fix: issue when energy_cost is not set

--- src/index.ts
+++ src/index.ts
@@ -124,7 +124,7 @@ export class SunsynkPowerFlowCard extends LitElement {
     const stateObj40 = this.hass.states[config.entities.non_essential_load1] || { state: '0' };
     const stateObj41 = this.hass.states[config.entities.non_essential_load2] || { state: '0' };
     const stateObj42 = this.hass.states[config.entities.essential_load1] || { state: '0' };
-    const stateObj43 = this.hass.states[config.entities.energy_cost] || { state: '' };
+    const stateObj43 = this.hass.states[config.entities.energy_cost] || { state: '', attributes: { unit_of_measurement: '' } };
     const stateObj44 = this.hass.states[config.entities.solar_sell_247] || { state: 'undefined' };
     const stateObj45 = this.hass.states[config.entities.essential_load2] || { state: '0' };
     const stateObj46 = this.hass.states[config.entities.pv_total] || { state: '0' };
@@ -1021,7 +1021,7 @@ export class SunsynkPowerFlowCard extends LitElement {
               <text id="dc_temp" x="110" y="266" class="${config.entities.dc_transformer_temp_90 === 'none' ? 'st12' : 'st3 left-align'}" fill="${inverter_colour}" display="${config.entities?.dc_transformer_temp_90 ? '' : 'none'}" >DC: ${stateObj38.state}°</text>
             </a>
             <a href="#" @click=${(e) => this.handlePopup(e, config.entities.energy_cost)}>
-              <text id="energy_cost" x="427" y="257"  class="${config.entities.energy_cost === 'none' ? 'st12' : 'st3 left-align'}" fill="${grid_colour}" display="${config.entities?.energy_cost ? '' : 'none'}" >${parseFloat(stateObj43.state).toFixed(2)}${stateObj43.attributes.unit_of_measurement || ''}</text>
+              <text id="energy_cost" x="427" y="257"  class="${config.entities.energy_cost === 'none' ? 'st12' : 'st3 left-align'}" fill="${grid_colour}" display="${config.entities?.energy_cost ? '' : 'none'}" >${parseFloat(stateObj43.state).toFixed(2)} ${stateObj43.attributes.unit_of_measurement}</text>
             </a>
           </svg>
         </div>
@@ -1304,7 +1304,7 @@ export class SunsynkPowerFlowCard extends LitElement {
               <text id="dc_temp" x="173" y="180.4" class="${config.entities.dc_transformer_temp_90 === 'none' ? 'st12' : 'st3 left-align'}" fill="${inverter_colour}" display="${config.entities?.dc_transformer_temp_90 ? '' : 'none'}" >DC: ${stateObj38.state}°</text>
             </a>
             <a href="#" @click=${(e) => this.handlePopup(e, config.entities.energy_cost)}>
-              <text id="energy_cost" x="105" y="195" class="${config.entities.energy_cost === 'none' ? 'st12' : 'st3 left-align'}" fill="${grid_colour}" display="${config.entities?.energy_cost ? '' : 'none'}" >${parseFloat(stateObj43.state).toFixed(2)}${stateObj43.attributes.unit_of_measurement || ''}</text>
+              <text id="energy_cost" x="105" y="195" class="${config.entities.energy_cost === 'none' ? 'st12' : 'st3 left-align'}" fill="${grid_colour}" display="${config.entities?.energy_cost ? '' : 'none'}" >${parseFloat(stateObj43.state).toFixed(2)} ${stateObj43.attributes.unit_of_measurement}</text>
             </a>
             ${config.entities?.pv_total
           ? svg`<a href="#" @click=${(e) => this.handlePopup(e, config.entities.pv_total)}>
