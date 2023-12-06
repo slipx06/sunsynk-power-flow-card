da4e2151e70eef1d80f1f858a1ff64b70592ed6a
Shaun J.V.Nieuwenhuizen <shaunjvn@gmail.com>
2023-12-06 08:52:37 +0200

    fix: undefined warning

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,6 @@
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import terser from "@rollup/plugin-terser";
 import json from '@rollup/plugin-json';
@@ -61,4 +61,12 @@ export default {
         clearScreen: false,
     },
     plugins: [...plugins],
+    onwarn: function (warning, handler) {
+        if (warning.code === 'THIS_IS_UNDEFINED') {
+            return;
+        }
+
+        // console.warn everything else
+        handler(warning);
+    }
 };
